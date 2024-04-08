5c209233c2227a0c2c3f0c98075826c2c001f961
pulindu2002 <vpulindu49@gmail.com>
2024-04-08 12:28:29 +0530

    cleark added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import Dashboard from './Dashboard';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme'; // Ensure this path is correct
-import { Container } from '@mui/material';
+import React from "react";
+import Dashboard from "./Dashboard";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme"; // Ensure this path is correct
+import { Container } from "@mui/material";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
   return (
-<ThemeProvider theme={theme}>
-  <CssBaseline />
-  <Container maxWidth={false} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-    <Dashboard />
-  </Container>
-</ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container
+        maxWidth={false}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Dashboard />
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Container>
+    </ThemeProvider>
   );
 }
 
